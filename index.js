58f57f26f4ab17e9b8e7dfba725890ceb4e72953
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-02-07 18:57:16 +0530

    Perform CRUD operation

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+const express = require("express")
 const mongoose = require("mongoose")
+const Students = require("./models/student")
+const app = express()
 
 mongoose.connect("mongodb://localhost:27017/school",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log("Connection Sucessfull")
+})
+
+app.get("/",(req,res)=>{
+    Students.find().then((student)=>{
+        res.json(student)
+    })
+})
+
+app.post("/add",(req,res)=>{
+    const student = new Students({
+        name:"Abhishek",
+        rollno:1,
+        branch:"CSE",
+        year:2020
+    })
+    student.save().then(()=>{
+        res.send("inserted")
+    })
+})
+
+app.put("/update",(req,res)=>{
+    Students.updateOne({name:"Abhishek"},{name:"Abhishek Garg"}).then(()=>{
+        res.send("updated")
+    })
+})
+
+app.delete("/delete",(req,res)=>{
+    Students.deleteOne({name:"Abhishek Garg"}).then(()=>{
+        res.send("deleted")
+    })
+})
+
+
+app.listen(3001,()=>{
+    console.log("server runnning on 3001")
 })
---
+++ models/student.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+const studentschema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    rollno:{
+        type:Number,
+        required:true
+    },
+    branch:{
+        type:String,
+        required:true
+    },
+    year:{
+        type:Number,
+        required:true
+    }
+})
+
+module.exports = mongoose.model("Students",studentschema)
